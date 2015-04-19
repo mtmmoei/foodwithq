5c8b4335173622849377a9fded50fd5e41a1ca87
Mimosha <mtmmoei@gmail.com>
2015-04-19 15:51:52 +0700

    create index.blade.php + link to css / js file create IndexController.php

---
+++ app/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <meta charset="utf-8">
+ <title>Food with Q</title>
+ {{HTML::script('https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js')}}
+ {{HTML::script('js/bootstrap.min.js')}}
+ {{HTML::script('js/index.js')}}
+ {{HTML::style('css/bootstrap.min.css')}}
+ {{HTML::style('css/index.css')}}
+</head>
+<body>
+  <h1>Hello World</h1>
+</body>
+</html>
