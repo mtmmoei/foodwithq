fb9fe8f19a65223073c5125fe5ff82487872b44c
Mimosha <mtmmoei@gmail.com>
2015-04-26 15:07:06 +0700

    ลองแชทได้

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -7,40 +7,14 @@
         	<div class="panel panel-default">
                 <div class="panel-heading top-bar">
                     <div class="col-md-8 col-xs-8">
-                        <h3 class="panel-title"><span class="glyphicon glyphicon-comment"></span> Chat - Q (bot)</h3>
+                        <h3 class="panel-title"><span class="glyphicon glyphicon-comment"></span> Chat - Quan (bot)</h3>
                     </div>
                     <div class="col-md-4 col-xs-4" style="text-align: right;">
-                    	<!-- refresh -->
-
-
-                     <!--    <a href="#"><span class="glyphicon glyphicon-remove icon_close" data-id="chat_window_1"></span></a> -->
+                        <a href="#"><span class="glyphicon glyphicon-refresh icon_refresh"></span></a>
                     </div>
                 </div>
                 <div class="panel-body msg_container_base">
                     <div class="row msg_container base_receive">
-                        <div class="col-md-2 col-xs-2 avatar">
-                            <img src="http://placehold.it/50/FA6F57/fff&text=Q" alt="User Avatar"  class=" img-responsive ">
-                        </div>
-                        <div class="col-md-10 col-xs-10">
-                            <div class="messages msg_receive">
-                                <p>that mongodb thing looks good, huh?
-                                tiny master db, and huge document store</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row msg_container base_sent">
-                        <div class="col-md-10 col-xs-10">
-                            <div class="messages msg_sent">
-                                <p>that mongodb thing looks good, huh?
-                                tiny master db, and huge document store</p>
-                            </div>
-                        </div>
-                        <div class="col-md-2 col-xs-2 avatar">
-                            <img src="http://placehold.it/50/55C1E7/fff&text=ME" alt="User Avatar"  class=" img-responsive ">
-                        </div>
-                    </div>
-                    
-                     <div class="row msg_container base_receive">
                         <div class="col-md-2 col-xs-2 avatar">
                             <img src="http://placehold.it/50/FA6F57/fff&text=Q" alt="User Avatar"  class=" img-responsive ">
                         </div>
