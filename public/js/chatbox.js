1f9a3fff68b87f8c806e5e1f8504ea58ec9c457c
Mimosha <mtmmoei@gmail.com>
2015-04-26 18:16:45 +0700

    ใส่ข้อความ

--- public/js/chatbox.js
+++ public/js/chatbox.js
@@ -1,17 +1,24 @@
-function generateSentHtml(message){
-return "<div class='row msg_container base_sent'>"+
-                    "<div class='col-md-10 col-xs-10'>"+
-                        "<div class='messages msg_sent'>"+
-                            "<p>"+message+"</p>"+
-                            "</div>"+
-                        "</div>"+
-                        "<div class='col-md-2 col-xs-2 avatar'>"+
-                            "<img src='http://placehold.it/50/55C1E7/fff&text=ME' alt='User Avatar'  class='img-responsive'>"+
-                        "</div>"+
-                    "</div>";
-}
-function generateReceiveHtml(message){
-  return "<div class='row msg_container base_receive'>"+
+var selectSection = false;
+var selectType = false;
+var selectSubtype = false;
+var selectCondition = true;
+var section = 1;
+var type = 2;
+var subtype = 3;
+var welcomeMsg = "1. เลือกตามย่าน\n"+
+    "2. เลือกของคาวของหวาน\n"+
+    "3. เลือกประเภทอาหาร";
+var selectSectionMsg = "1. สยาม\n"+
+    "2. สามย่าน\n"+
+    "3. สีลม";
+
+var person = [];
+person[0] = "John";
+person[1] = "Doe";
+person[2] = 46;
+function reply(message){
+   var chat_container = $(".msg_container_base");
+   var html = "<div class='row msg_container base_receive'>"+
                         "<div class='col-md-2 col-xs-2 avatar'>"+
                             "<img src='http://placehold.it/50/FA6F57/fff&text=Q' alt='User Avatar'  class='img-responsive'>"+
                         "</div>"+
@@ -21,30 +28,57 @@ function generateReceiveHtml(message){
                             "</div>"+
                         "</div>"+
                     "</div>";
+   chat_container.append(html);
+   chat_container.scrollTop(chat_container.prop("scrollHeight"));
 }
-function reply(){
-  var message = "that mongodb thing looks good, huh? tiny master db, and huge document store";
+
+function ask(message){
    var chat_container = $(".msg_container_base");
-   chat_container.append(generateReceiveHtml(message));
+   var html = "<div class='row msg_container base_sent'>"+
+                    "<div class='col-md-10 col-xs-10'>"+
+                        "<div class='messages msg_sent'>"+
+                            "<p>"+message+"</p>"+
+                            "</div>"+
+                        "</div>"+
+                        "<div class='col-md-2 col-xs-2 avatar'>"+
+                            "<img src='http://placehold.it/50/55C1E7/fff&text=ME' alt='User Avatar'  class='img-responsive'>"+
+                        "</div>"+
+                    "</div>";
+   chat_container.append(html);
    chat_container.scrollTop(chat_container.prop("scrollHeight"));
 }
 
 $(document).on('click', '#btn-chat', function (e) {
-    // var $this = $(this);
     var message = $(".chat_input").val();
-   var chat_container = $(".msg_container_base");
-   chat_container.append(generateSentHtml(message));
-   chat_container.scrollTop(chat_container.prop("scrollHeight"));
+   ask(message);
+   if(selectCondition){
+    selectCondition = false;
+    if(message.trim()==section){
+        selectSection=true;
+        reply("1");
+    } else if(message.trim()==type){
+        selectType=true;
+        reply("2");
+    } else if(message.trim()==subtype){
+        selectSubtype=true;
+        reply("3");
+    } else {
+        reply("ใส่หมายเลขมาเลย");
+        selectCondition = true;
+    }
+   }
    $(".chat_input").val('');
-   reply();
 });
 
 $(document).on('click', '.icon_refresh', function (e) {
-  var message = "that mongodb thing looks good, huh? tiny master db, and huge document store";
-$(".msg_container_base").html(generateReceiveHtml(message));
+  selectCondition = true;
+  $(".msg_container_base").html("");
+  reply(welcomeMsg);
 });
 
 $(document).ready(function() {
+    $("#chat_result").hide();
+  reply(welcomeMsg);
   $('.chat_input').keypress(function (e) {
     var key = e.which;
     if(key == 13)  // the enter key code
