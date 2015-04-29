7a6e3ef52baac9d1eb0a7f8b50609d90ff9b092f
quanruthai <quanruthai.t@gmail.com>
2015-04-29 21:50:42 +0700

    edit nav

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -16,7 +16,7 @@
 		<div class="container-fluid">
 			<!-- Brand and toggle get grouped for better mobile display -->
 			<div class="navbar-header">
-				<a class="navbar-brand" href="#">ของกินของขวัญ</a>
+{{ HTML::link('/','ของกินของขวัญ', array('class' => 'navbar-brand'))}}
 			</div>
 
 			<!-- Collect the nav links, forms, and other content for toggling -->
