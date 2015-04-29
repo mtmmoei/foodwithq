52a4fce49212680c1227c024ec65e331c5ee4b34
Mimosha <mtmmoei@gmail.com>
2015-04-29 17:19:27 +0700

    modal bug

--- app/models/Resturant.php
+++ app/models/Resturant.php
@@ -14,41 +14,41 @@ class Resturant extends Eloquent
 	}
 public static function getRestaurant($type,$section,$subtype){
 		if(!empty($type) && !empty($section) && !empty($subtype)){
-Log::info("test db: 1" );
+//Log::info("test db: 1" );
 			return DB::table('resturant')->where('type', '=',$type)->where('section', '=',$section)->where('subtype', '=',$subtype)->get();
 
 
 		}
 		if(!empty($type) && !empty($section)){
-Log::info("test db: 2" );
+//Log::info("test db: 2" );
 			return  DB::table('resturant')->where('type', '=',$type)->where('section', '=',$section)->get();
 
 
 		}
 		if(!empty($type) && !empty($subtype)){
-Log::info("test db: 3" );
+//Log::info("test db: 3" );
 		return DB::table('resturant')->where('type', '=',$type)->where('subtype', '=',$subtype)->get();
 
 		}
 		if(!empty($section) && !empty($subtype)){
-Log::info("test db: 4" );
+//Log::info("test db: 4" );
 			return  DB::table('resturant')->where('section', '=',$section)->where('subtype', '=',$subtype)->get();
 
 
 		}
 		if (!empty($subtype)) {
-			Log::info("test db: 5" );
+//			Log::info("test db: 5" );
 		return  DB::table('resturant')->where('subtype', '=',$subtype)->get();
 
 		}
 
 		if (!empty($section)) {
-			Log::info("test db: 6" );
+//			Log::info("test db: 6" );
 			return  DB::table('resturant')->where('section', '=',$section)->get();
 
 		}
 		if (!empty($type)) {
-			Log::info("test db: 7" );
+//			Log::info("test db: 7" );
 			return DB::table('resturant')->where('type', '=',$type)->get();
 
 		}
