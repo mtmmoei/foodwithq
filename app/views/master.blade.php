b2f58b16431c2ca0e2e1866eead61981e2cbd832
quanruthai <quanruthai.t@gmail.com>
2015-04-29 21:03:57 +0700

    move nev to master

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -10,6 +10,22 @@
 @yield('style')
 </head>
 <body>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">ของกินของขวัญ</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li>{{ HTML::link('/about','About')}}</li>
+      </ul>
+    </div>
+
+  </div>
+</nav>
 @yield('content')
 </body>
 <footer>
