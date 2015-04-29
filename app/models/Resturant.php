6715dccf2d88f67657498aa2c3c13c2af54c3669
quanruthai <quanruthai.t@gmail.com>
2015-04-29 15:27:56 +0700

    get restaurant all combination

--- app/models/Resturant.php
+++ app/models/Resturant.php
@@ -24,6 +24,26 @@ class Resturant extends Eloquent
 			return $data;
 
 		}
+		if(!empty($type) && !empty($subtype)){
+
+			$data = DB::table('resturant')->where('type',$type)->where('section',$section)->get();
+			var_dump($data);
+			return $data;
+
+		}
+		if(!empty($section) && !empty($subtype)){
+
+			$data = DB::table('resturant')->where('section',$section)->where('subtype',$subtype)->get();
+			var_dump($data);
+			return $data;
+
+		}
+		if (!empty($subtype)) {
+			$data = DB::table('resturant')->where('subtype',$subtype)->get();
+			var_dump($data);
+			return $data;
+		}
+
 		if (!empty($section)) {
 			$data = DB::table('resturant')->where('section',$section)->get();
 			var_dump($data);
